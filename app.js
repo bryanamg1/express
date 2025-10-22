0a8eeb4ba4a8930c44a3ee2e4e832479e6bff128
bryan marquez <bryan_amg1@icloud.com~>
2025-10-22 19:29:52 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
+dotenv.config();
 
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(
+    `<h1 style="color: blue; text-align: center; margin-top: 20%;">Welcome to the Express Server!</h1>`
+  );
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ID requested: ${userId}`);
 });
+
+app.get('/search', (req, res) => {
+  const query = req.query.q;
+  res.send(`Search query: ${query}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
 
